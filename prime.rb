3b71488b33270715d43b7fb962c65a6222947cd4
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-08-25 20:44:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime?(n)
+  prime_array = []    
+p = 2
+if n < 2
+    return p
+end
+
+while (n % p == 0) && (p < n)
+    prime_array.push(p)
+    p += 1
+end
+#puts prime_array.inspect
+return prime_array
+end
+#prime_factors(600851475143)
+puts prime_factors(30)
+  
+end
