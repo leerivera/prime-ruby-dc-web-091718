bf8ceaa52d3f5c064d3ceb321a1eae50df2d66bb
leerivera80 Rivera <leerivera80@gmail.com>
2018-09-17 04:17:18 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,19 +1,25 @@
 # Add  code here!
-def prime?(n)
-  prime_array = []    
-p = 2
-if n < 2 #any even # over 2 will not be a prime #
-    return p
-end
+#def prime?(n)
+  #prime_array = []    
+####end
 
-while (n % p == 0) && (p < n)
+#while (n % p == 0) && (p < n)
     prime_array.push(p)
-    p += 1
-end
+    #p += 1
+#end
 
-return prime_array
-end
+#return prime_array
+#end
 
-puts prime?(30)
+#puts prime?(30)
   
+#end
+def prime?(num)
+  return false if num < 2 
+  return true if num == 3 || num == 2 
+    if (2...num-1).any?{|i| num % i == 0}
+      false
+    else
+      true
+    end
 end
